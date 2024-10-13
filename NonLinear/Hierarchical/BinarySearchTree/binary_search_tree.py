8bd5f2599845ff79ba5b2c73465e6679777dd84a
Noah <pedromilarski11@gmail.com>
2024-10-13 15:11:47 -0300

    FEATURE: Addition of first implementations of Binary Search Tree

---
+++ NonLinear/Hierarchical/BinarySearchTree/binary_search_tree.py
@@ -0,0 +1,58 @@
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    def inorder_traversal(self):
+        if self.left:
+            self.left.inorder_traversal()
+        print(self.value)
+        if self.right:
+            self.right.inorder_traversal()
+
+    def preorder_traversal(self):
+        print(self.value)
+        if self.left:
+            self.left.preorder_traversal()
+        if self.right:
+            self.right.preorder_traversal()
+
+    def postorder_traversal(self):
+        if self.left:
+            self.left.postorder_traversal()
+        if self.right:
+            self.right.postorder_traversal()
+        print(self.value)
+
+
+b = BinarySearchTree(10)
+b.insert(2)
+b.insert(6)
+b.insert(3)
+b.insert(9)
+b.insert(6)
+b.insert(9)
+b.insert(3)
+b.insert(11)
+b.insert(18)
+
+b.inorder_traversal()
+print("--------------------------")
+
+b.preorder_traversal()
+print("--------------------------")
+
+b.postorder_traversal()
